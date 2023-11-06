59d7e40f2726167c7fca09b3ce77cd07cd1c6dca
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2023-11-06 16:07:08 +0700

    nambah border dan shadownya biar mirip pmb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,11 @@ import ui_logo from './ui_logo.jpg'
 
 export default function Home() {
   return (
-    <>
-    <header className="">
-      <nav className='flex p-4'>
-        <h1 className='arial mx-5'>SBF BISMIT</h1>
-      </nav>
-    </header>
-    <body>
-
-    </body>
-    </>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <nav className="w-full h-20 bg-white border border-slate-300 shadow-md flex items-center justify-between">
+          <p className='text-2xl font-bold cursor-pointer m-3'>LOGO</p>
+          <button className=''>Home</button>
+        </nav>
+      </main>
   )
 }
